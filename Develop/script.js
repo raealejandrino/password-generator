2bd53e5ed2073da01e609a511619732984e9e3fb
Rae Alejandrino <raealejandrino@gmail.com>
2021-10-05 17:24:59 -0400

    Added charset variables

--- Develop/script.js
+++ Develop/script.js
@@ -34,6 +34,21 @@ var randomNumber = function(min,max) {
 }
 
 
+// Charset vars
+
+// 26 in alphabet
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// 10 in numbers
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// 8 specials
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+
+
+
 
 
 
