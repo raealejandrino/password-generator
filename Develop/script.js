886d46d305258d07c54cea73c2d12f3d3e977e66
Rae Alejandrino <raealejandrino@gmail.com>
2021-10-05 17:19:21 -0400

    More psuedocode, added random number function

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,37 @@
 // Assignment code here
 
+
+// Implement window prompt for length of password
+
+// Store that value in a variable that will be used in the condition expression of iterator 
+
+// must create randomizer for each criteria?
+
+// create array for all lowercase letters, all uppercase letters, all numbers, all special characters
+
+// make use of random number function
+
+
+
+////////////////////////////////////
+
+
 // create criteria functions that include prompts with validation and stores the value in a variable
 
 // create array with each criteria function assigned its own index
 
 // run for loop that runs each criteria function and prints the value into a string
+
+
+
+
+// random number
+
+var randomNumber = function(min,max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+}
 
 
 
