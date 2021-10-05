5bf45a5c0021b1cfd598b6b1e534d0396d4e5a47
Rae Alejandrino <raealejandrino@gmail.com>
2021-10-05 14:06:23 -0400

    Initial pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
 
+// create criteria functions that include prompts with validation and stores the value in a variable
+
+// create array with each criteria function assigned its own index
+
+// run for loop that runs each criteria function and prints the value into a string
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
